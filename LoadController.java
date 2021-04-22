69a684c1ca9aea83036f80eeb3f67267ead4651c
trongnguyen42 <52287335+trongnguyen42@users.noreply.github.com>
2021-04-23 01:26:05 +0700

    Add files via upload

---
+++ LoadController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dht.saleapp;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Admin
+ */
+public class LoadController implements Initializable{
+
+    
+    @FXML
+    void clicked(ActionEvent event){
+    
+      JFXButton btn = new JFXButton();
+      btn.setStyle("-fx-background-color: #F7B5B0;");
+      
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
